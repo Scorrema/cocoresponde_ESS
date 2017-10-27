d18bf2e6a8a87290b5d7f29892e5082fc921afd9
edd3010 <32990854+edd3010@users.noreply.github.com>
2017-10-26 20:05:14 -0500

    identacion js
    organizacion en el js

--- js/main.js
+++ js/main.js
@@ -3,69 +3,68 @@ var bandera = false;
 var letra = "";
 var respuesta = "";
 
-function pregunta(event){
-    
-
-event.preventDefault();
-event.stopPropagation();
-captura = event.key
-
-var frase = ["c", "o", "c", "o", " ", "r","e", "s", "p","o","n","d","e"," ","e","s","t","a"," ","p","r","e","g","u","n","t","a"," ","p","o","r"," ","f","a","v","o","r"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "];
+    function pregunta(event){
 
 
-    
-if(captura == "."){
-    
-    
-    bandera = !bandera;
-}
-    
-    if(contador <= frase.length && bandera == true){
-        letra = letra+frase[contador];
-        contador = contador+1;
+        event.preventDefault();
+        event.stopPropagation();
+        captura = event.key
+
+
+        var frase = "coco responde esta pretunta";
+
+
+            if(captura == "."){
+                bandera = !bandera;
+                }
+
+            if(contador <= frase.length && bandera == true){
+                letra = letra+frase.charAt(contador);
+                contador = contador+1;
+
+
+            if(captura != "."){
+                respuesta = respuesta+event.key;
+                console.log(respuesta)
+                }
+
+
+
+             $("#peticion").val(letra);
+
+                }
+
+
+            if(bandera != true && captura != "."){
+
+                letra = letra+event.key;  
+                }
+
+
+            $("#peticion").val(letra);
+            }
+
+    function enviar(){
+
+        var texto = $("#pregunta").val();
+        console.log(texto)
         
-        
-    if(captura != "."){
-        respuesta = respuesta+event.key;
-        console.log(respuesta)
-        }
-        
+            if(texto.length < 2){
+            alert("Ingrese una pregunta valida")
+                }
+                    else{
 
- 
-     $("#peticion").val(letra);
-            
-}
-    
+                        $("#txt").html(respuesta);
+                        }
 
-if(bandera != true && captura != "."){
-    
-    letra = letra+event.key;  
-}
-    
-    
-$("#peticion").val(letra);
-}
-
-function enviar(){
-      
-    var texto = $("#pregunta").val();
-    console.log(texto)
-    if(texto.length < 2){
-        alert("Ingrese una pregunta valida")
-    }
-    else{
-        
-      $("#txt").html(respuesta);
-    }
-    
-}
+            } 
 
 
-var container = document.getElementById('container');
+        var container = document.getElementById('container');
 
-setTimeout(function() {
-    container.classList.add('cerrar');
-    
-    document.body.style.overflowY= "visible";// despueés de cargar le devolvemos el scroll
-}, 9000);
+            setTimeout(function() {
+            container.classList.add('cerrar');
+            document.body.style.overflowY= "visible";// despueés de cargar le devolvemos el scroll
+            }
+            , 9000);
 
