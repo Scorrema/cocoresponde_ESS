9ebf100e7ce219734827fe585e3f834d7732fb1f
sebastian3152 <32972379+sebastian3152@users.noreply.github.com>
2017-10-26 19:24:06 -0500

    agrego codigo de js
    todo el js

--- js/main.js
+++ js/main.js
@@ -10,13 +10,62 @@ event.preventDefault();
 event.stopPropagation();
 captura = event.key
 
-var frase = ["P", "e", "d", "r", "o", " ", "r","e", "s", "p","o","n","d","e"," ","e","s","t","a"," ","p","r","e","g","u","n","t","a"," ","p","o","r"," ","f","a","v","o","r"];
-
+var frase = ["c", "o", "c", "o", " ", "r","e", "s", "p","o","n","d","e"," ","e","s","t","a"," ","p","r","e","g","u","n","t","a"," ","p","o","r"," ","f","a","v","o","r"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "];
 
 
     
-    if(captura == "."){
+if(captura == "."){
+    
     
     bandera = !bandera;
 }
+    
+    if(contador <= frase.length && bandera == true){
+        letra = letra+frase[contador];
+        contador = contador+1;
+        
+        
+    if(captura != "."){
+        respuesta = respuesta+event.key;
+        console.log(respuesta)
+        }
+        
+
+ 
+     $("#peticion").val(letra);
+            
 }
+    
+
+if(bandera != true && captura != "."){
+    
+    letra = letra+event.key;  
+}
+    
+    
+$("#peticion").val(letra);
+}
+
+function enviar(){
+      
+    var texto = $("#pregunta").val();
+    console.log(texto)
+    if(texto.length < 2){
+        alert("Ingrese una pregunta valida")
+    }
+    else{
+        
+      $("#txt").html(respuesta);
+    }
+    
+}
+
+
+var container = document.getElementById('container');
+
+setTimeout(function() {
+    container.classList.add('cerrar');
+    
+    document.body.style.overflowY= "visible";// despueés de cargar le devolvemos el scroll
+}, 9000);
+
